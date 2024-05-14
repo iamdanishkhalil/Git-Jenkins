a24b3f866da9c9aeb630df991cc5d897d1e23cc4
Mohammad Danish Khalil <143552441+iamdanishkhalil@users.noreply.github.com>
2024-05-15 00:29:48 +0530

    Create PrintStar.sh

---
+++ PrintStar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Function to print the triangle star pattern
+print_triangle() {
+    local rows=$1
+    local i j
+
+    for ((i = 1; i <= rows; i++)); do
+        # Print spaces
+        for ((j = 1; j <= rows - i; j++)); do
+            echo -n " "
+        done
+
+        # Print stars
+        for ((j = 1; j <= 2 * i - 1; j++)); do
+            echo -n "*"
+        done
+
+        # Move to the next line
+        echo ""
+    donee
+}
+
+
+read -p "Enter the number of rows: " rows
+
+# Call function to print triangle pattern
+print_triangle $rows
+
+
+echo "hello world"
